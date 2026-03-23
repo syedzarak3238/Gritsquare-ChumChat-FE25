106792781f3112a1ad333f46fbe8e63f53f42973
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-23 14:39:23 +0100

    Can now reply, saves in DB

--- javascript/MessageFunctions/sendReply.js
+++ javascript/MessageFunctions/sendReply.js
@@ -1,14 +1,19 @@
-import { reference, db, messagesRef, repliesRef } from "./firebase.js";
+import { auth, reference, db, messagesRef, repliesRef } from "../firebase.js";
 import { push, set, onValue, remove, ref, get } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 export const sendReply = (messageId, text) => {
+    const user = auth.currentUser;
+
+
     const newReply = push(repliesRef);
     const replyData = {
         message: text,
         message_id: messageId,
-        user_id: currentUser.user_id,
+        user_id: user.uid,
         timestamp: Date.now()
     }
+
+    console.log(replyData)
 
     set(newReply, replyData);
 }
